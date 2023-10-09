6d528a21d0d9c17e81b2befe6d7db3cac5183a02
Helena Shih Chapman - slurpee waves <42580758+helenashih@users.noreply.github.com>
2023-10-09 19:52:50 -0400

    added array code samples

--- script.js
+++ script.js
@@ -38,3 +38,45 @@ const randomID = (() => {
     
     return () => gen.next().value;
   })()
+
+
+  // Basic array samples
+
+  const fruits = ['apple', 'peach', 'orange'];
+  const berries = ['strawberry', 'blueberry', 'raspberry'];
+
+  // concatenate arrays
+
+  let allFruits = fruits.concat(berries);
+
+  // or use a spread operator to concatenate
+
+  allFruits = [...fruits, ...berries];
+  console.log(`use a spread operator to flatten the array ${allFruits}`);
+
+  // or use flatten method
+
+  allFruits = [...fruits, berries, 2, 3, [4, 5], 6, 7, [8, 9, 10]];
+
+  let arr = allFruits.flat();
+
+  console.log(arr);
+
+  // static methods on array objects Array.isArray, Array.from('12345') or Array.of()
+
+  // create an array that reverse and add an additional element at the end
+arr = [1, 2, 3, 4, 5];
+arr.unshift(0);
+arr.push(6);
+
+console.log(arr.reverse());
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+// concatenate arr1 and arr2 but remove duplicate 5
+
+arr = [...arr1.slice(0, 4), ...arr2];
+console.log(arr);
+
+
