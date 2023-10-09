1e3f68b13abac849647cfda9e52369cfe7e78673
Helena Shih Chapman - slurpee waves <42580758+helenashih@users.noreply.github.com>
2023-10-09 17:27:14 -0400

    Add more samples

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ myNewString = myString[0].toUpperCase() + myString.substring(1);
 myNewString = `${myString[0].toUpperCase()}${myString.substring(1)}`;
 
 console.log(myNewString);
+
+// create a variable x that is a random number between 1 and 100 and variable y between 1 and 50
+
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+
+console.log(`Sum of x and y ${x} + ${y} = ${x+y}`);
+console.log(`Diff of x and y ${x} - ${y} = ${x-y}`);
+console.log(`Product of x and y ${x} * ${y} = ${x*y}`);
+console.log(`Quotient of x and y ${x} / ${y} = ${x/y}`);
+console.log(`Remainder of x and y ${x} % ${y} = ${x%y}`);
